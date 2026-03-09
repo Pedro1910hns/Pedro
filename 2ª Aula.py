bfa08d5e24fe81e0c4226cadf45ed5003b35e19e
Pedro1910hns <pedro.nasc2121@gmail.com>
2026-03-09 20:32:51 -0300

    2ª Aula

---
+++ 2ª Aula.py
@@ -0,0 +1,47 @@
+# Meu primeiro programa
+print ("Hello world")
+
+check1 = 8
+check2 = 5.5
+
+print (check1+check2)
+
+soma = check1+check2
+print (soma)
+
+media = (check1+check2)/2
+print (media)
+
+print(type(check1))
+
+
+nome = "Pedro"
+print(type (nome))
+
+#1
+print("pedro")
+
+#2
+a = 3
+b = 5
+resultado = (2*a*3*b)
+print (resultado)
+
+#3
+check1 = 8
+check2 = 5.5
+check3 = 9
+print(check3+check2+check1)
+
+
+media = (check1+check2)/2
+print(f"Média = {media:.1f}")
+
+resto = 10 % 3
+print(resto)
+print()
+
+check1 = float(input("Checkpoint 1 = "))
+check2 = float(input("checkpoint 2 = "))
+media = (check1 + check2)/2
+print(f"Média = {media:.1f}")
